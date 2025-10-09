f392555345d968d2f39286c56c733fc82b391ae2
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-10-09 21:04:32 +0100

    Update __init__.py

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,1 +1,15 @@
+"""
+Plots module for Glass Coating Analysis Tool
+"""
 
+from .scatter_plot import create_scatter_plot
+from .bar_chart import create_bar_chart
+from .radial_plots import create_radial_plots
+from .parallel_coordinates import create_parallel_coordinates
+
+__all__ = [
+    'create_scatter_plot',
+    'create_bar_chart',
+    'create_radial_plots',
+    'create_parallel_coordinates'
+]
